ceb6ee027389f9c87e360240a6a9ece60b4f480e
eyhdev <alaqsa335@gmail.com>
2023-08-08 20:46:01 +0300

    Initial Commit

---
+++ Animation/AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationApp.swift
+//  Animation
+//
+//  Created by eyh.mac on 8.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
