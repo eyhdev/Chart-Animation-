d698929bb56cbd384ecc33b85285a90ce5ad07c6
eyhdev <alaqsa335@gmail.com>
2023-08-09 14:16:00 +0300

    Chart Animation

--- Animation/ContentView.swift
+++ Animation/ContentView.swift
@@ -8,14 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var isShowing = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack{
+            Color("color")
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack{
+                HStack(spacing: 20){
+                    A(value: isShowing ? 200 : 50)
+                    A(value: isShowing ? 250 : 150)
+                    A(value: isShowing ? 300 : 100)
+                    A(value: isShowing ? 200 : 50)
+                    A(value: isShowing ? 200 : 250)
+                    A(value: isShowing ? 200 : 150)
+                    A(value: isShowing ? 200 :250)
+                    A(value: isShowing ? 200 : 50)
+                }.padding(.top, -10)
+                    .offset(y: -100)
+                    .animation(Animation.linear(duration: 1).repeatForever())
+                    .onTapGesture { self.isShowing.toggle()
+                        
+                    }
+            }
         }
-        .padding()
+    }
+    
+    struct A: View{
+        
+        var value: CGFloat = 0
+        
+        var body: some View{
+            VStack{
+                ZStack(alignment: .bottom){
+                    Capsule().frame(width: 25, height: 300)
+                        .foregroundColor(Color.white)
+                    Capsule().frame(width: 25, height: value)
+                        .foregroundColor(Color.orange)
+                }
+            }
+        }
     }
 }
 
